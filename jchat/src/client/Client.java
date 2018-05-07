419d3de7d38256bfe0e40c0b6116f2471e30a9d8
Aleandro1 <Aleandro1@Aleandro>
2018-05-07 19:14:45 +0200

    Changed Connectivity

--- jchat/src/client/Client.java
+++ jchat/src/client/Client.java
@@ -3,6 +3,7 @@ package client;
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import utils.*;
 
 /* TODO Scegliere una porta e un metodo di identificazione tra i peer
  * Porta default: 9876
@@ -11,33 +12,44 @@ import java.util.*;
  * Thread Demon aggiornamento IP contatti
  */
 
-public class Client {
+public class Client extends Thread{
 
-	public static void main(String[] args) {
+	@Override
+	public void run() {
 		// Dichiarazione variabili
 		Scanner sc = new Scanner(System.in);
 		ciclo1:
 		while (true) {
 			System.out.println("Benvenuto!\n1 - Sign In\n2 - Sign Up");
 			String choice1 = sc.nextLine();
+			try {
+				Socket s = new Socket(InetAddress.getByName(Connectivity.SERVER_HOSTNAME),Connectivity.PORT);
+			} catch (Exception e) {
+				System.err.println(e);
+			}
 			if (!choice1.matches("[1-2]")) {
 				System.out.println("java.jchat.client.Client: Syntax Error");
 				continue ciclo1;
 			}
 			if(choice1.equals("1")) {
-				//Sign In
+				String signInLine = sc.nextLine();
+				if (signInLine.matches(Connectivity.logInRegex)) {
+				} else {
+					System.out.println("java.jchat.client.Client: Syntax Error");
+					continue ciclo1;
+				}
+				//Invio richiesta di sign In al Server
+				
+				//Ricezione risposta di sign In dal Server
 			}
 			else if(choice1.equals("2")) {
 				//Sign Up
 			}
-			String line1 = sc.nextLine();
-			if (line1.matches(utils.Connectivity.logInRegex)) {
-			} else {
-				System.out.println("java.jchat.client.Client: Syntax Error");
-				continue ciclo1;
-			}
 		}
-
 	}
-
+	
+	public static void main(String[] args) {
+		new Client().start();
+	}
+	
 }
--- jchat/src/utils/Connectivity.java
+++ jchat/src/utils/Connectivity.java
@@ -4,5 +4,7 @@ public class Connectivity {
 	public static final int PORT=9876;
 	public static String logInRegex = "[\\w]+[ ][\\w]+";
 	public static String signUpRegex = "[\\w]+[ ][\\w]+[ ][\\w]";
+	public static String SERVER_IP = "160.97.146.187";
+	public static String SERVER_HOSTNAME = "jchat.homepc.it";
 	private Connectivity() {};
 }
