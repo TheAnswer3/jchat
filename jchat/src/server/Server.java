63c60897e7687114e4592469b1d284e2526c26ef
hat-toor <hat-toor@localhost>
2018-05-07 17:47:56 +0200

    Server class and utils package added.

--- jchat/src/server/Server.java
+++ jchat/src/server/Server.java
@@ -1,5 +1,14 @@
 package server;
-
+import java.net.*;
+import java.io.*;
+import java.util.*;
+/* TODO
+ * Porta default: 9876
+ * Mappa nick-password, permanente.
+ * Mappa nick-IP, non permanente.
+ * 
+ */
 public class Server {
-
+	static final int PORT=9876;
+	
 }
---
+++ jchat/src/utils/Connectivity.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Connectivity {
+	public static final int PORT=9876;
+	private Connectivity() {};
+}
